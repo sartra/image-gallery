c51a4ab45643a697700d9ddbbb36269be52afebe
sartra <strenshaw@gmail.com>
2018-06-07 23:48:39 -0400

    cleanup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 export default function images(state = { 
   images: [], 
-  metadata: [{ title: 'Tall trees', description: 'Green trees in the north west' }, { title: 'Tree in Lake', description: 'experimental photography' }, { title: 'Not a Rose', description: 'This is definitely not a rose' }, { title: 'Misty Mountains', description: 'Wish I were there' }, { title: 'Sunflower', description: 'Summertime' }  ]
+  metadata: [{ title: 'Bamboo', description: 'Beautiful invasive species' }, { title: 'Tree in Lake', description: 'experimental photography' }, { title: 'Not a Rose', description: 'This is definitely not a rose' }, { title: 'Misty Mountains', description: 'Wish I were there' }, { title: 'Sunflower', description: 'Summertime smells like this' }  ]
 }, action) {
 
   switch (action.type) {
@@ -9,17 +9,13 @@ export default function images(state = {
     case 'LOAD_IMAGES_FAILURE':
       return state;
     case 'SELECT_IMAGE':
-      let path = action.image.slice(-12, -4) // unique path for routes 
-      let image = action.image; // selected image url 
-      const images = {...state}.images  // array of all images 
-      const metadata = { ...state }.metadata // array of titles 
-      // get index of action.image in the state.images array and then set titles[index] as the title prop 
+      let path = action.image.slice(-12, -4) 
+      let image = action.image; 
+      const images = {...state}.images  
+      const metadata = { ...state }.metadata 
       let number = images.indexOf(image)+1; 
       let title = getTitle(image, images, metadata)
-      console.log('number', number, 'title: ', title)
       let description = getDescription(image, images, metadata)
-      console.log('description:', description)
-     console.log('path', path)
       return {...state, selectedImage: action.image, routePath: path, number: number, title: title, description: description};
     default:
       return state;
