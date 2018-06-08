76bea983fcb7d9ee21602a9fa329963f5233da80
sartra <strenshaw@gmail.com>
2018-06-07 22:27:08 -0400

    changed library

--- src/api/flickr.js
+++ src/api/flickr.js
@@ -1,6 +1,6 @@
 const API_KEY = 'a46a979f39c49975dbdd23b378e6d3d5';
 
-const API_ENDPOINT = `https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${API_KEY}&gallery_id=72157667808054427&format=json&nojsoncallback=1&per_page=5`
+const API_ENDPOINT = `https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${API_KEY}&gallery_id=72157697754618575&format=json&nojsoncallback=1&per_page=5`
 
 
 export const fetchImages = () => {
