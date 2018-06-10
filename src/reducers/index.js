90c36ddc38e9d4f761207c81d81432d9138f72a1
sartra <strenshaw@gmail.com>
2018-06-10 18:28:56 -0400

    using connected router to push to history

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,7 @@
+import { push } from 'connected-react-router'
+import createHistory from 'history/createBrowserHistory'
+
+
 export default function images(state = { 
   images: [], 
   metadata: [{ title: 'Bamboo', description: 'Beautiful invasive species' }, { title: 'Tree in Lake', description: 'experimental photography' }, { title: 'Not a Rose', description: 'This is definitely not a rose' }, { title: 'Misty Mountains', description: 'Wish I were there' }, { title: 'Sunflower', description: 'Summertime smells like this' }  ]
@@ -9,14 +13,35 @@ export default function images(state = {
     case 'LOAD_IMAGES_FAILURE':
       return state;
     case 'SELECT_IMAGE':
-      let path = action.image.slice(-12, -4) 
+      // let path = action.image.slice(-12, -4) 
       let image = action.image; 
       const images = {...state}.images  
       const metadata = { ...state }.metadata 
       let number = images.indexOf(image)+1; 
-      let title = getTitle(image, images, metadata)
-      let description = getDescription(image, images, metadata)
-      return {...state, selectedImage: action.image, routePath: path, number: number, title: title, description: description};
+      let title = getTitle(image, images, metadata);
+      let description = getDescription(image, images, metadata);
+      let i = images.indexOf(image); // use as route
+      let path = `image${i}`
+      const history = createHistory()
+      // Get the current location.
+      const location = history.location
+      // Listen for changes to the current location.
+      const unlisten = history.listen((location, action) => {
+        // location is an object like window.location
+        console.log(action, location.pathname, location.state)
+      })
+
+      // Use push, replace, and go to navigate around.
+      history.push(`/${path}`, { ...state,
+        selectedImage: action.image,
+        routePath: i,
+        number: number,
+        title: title,
+        description: description
+      })
+      unlisten();
+      //history.push(`/${i}`)
+      return {...state, selectedImage: action.image, routePath: i, number: number, title: title, description: description};
     default:
       return state;
   }
